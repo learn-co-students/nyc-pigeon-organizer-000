b83db34c656e549e90d8056c4913ad1450a0f712
Artem Eremin <artross@yandex.ru>
2015-06-21 22:53:37 +0300

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(attr, val_hash), res|
+    val_hash.each do |val, names|
+	  names.each do |name| 
+	    if res.has_key?(name) then
+		  if res[name].has_key?(attr) then
+		    res[name][attr] << val.to_s
+          else
+            res[name][attr] = [val.to_s]
+          end			
+		else  
+		  res[name] = { attr => [val.to_s] } 
+		end  
+	  end
+	end
+  end
 end
