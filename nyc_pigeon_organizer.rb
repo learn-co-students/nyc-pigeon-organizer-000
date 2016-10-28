ba9bb6d94f9eb21f9508f71f990b09d6cb6bf7ce
Lucas Fields <lqf2b8@gmail.com>
2016-10-28 16:41:41 +0000

    complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |label, info|
+    info.each do |details, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][label] ||= []
+        new_hash[name][label] << details.to_s
+      end
+    end
+  end
+  new_hash
 end
+
