7f1a1e50d9eae3d87ec12980dbd22562773532ff
OnlyHughMan <hughvidler@hotmail.com>
2017-05-20 17:39:33 +1000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+pigeon_data = {
+	:color => {
+  		:purple => ["Theo", "Peter Jr.", "Lucky"],
+  		:grey => ["Theo", "Peter Jr.", "Ms. K"],
+  		:white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+  		:brown => ["Queenie", "Alex"]
+},
+	:gender => {
+ 		:male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+ 		:female => ["Queenie", "Ms. K"]
+},
+	:lives => {
+  		"Subway" => ["Theo", "Queenie"],
+  		"Central Park" => ["Alex", "Ms. K", "Lucky"],
+  		"Library" => ["Peter Jr."],
+  		"City Hall" => ["Andrew"]
+	}
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  	hash = {}
+  	data.each do |set, step| # set is color gender lives, step is the hashes and keys contained
+  		step.each do |subset, list| # subset is the hash, list is the key each of which is an array
+  			list.each do |name|	# each name in the array
+  				hash[name] = {} unless hash[name]
+  				hash[name][set] = [] unless hash[name][set]
+  				hash[name][set] << subset.to_s
+  			end
+		end
+  	end
+  	return hash
 end
+
+#nyc_pigeon_organizer(pigeon_data)
